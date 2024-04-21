a3a3c15636b59ebdbbe7fd04514bddf74bbffff2
Tsilavina <tsilavinarj02@gmail.com>
2024-04-21 17:49:13 +0000

    Add modules folder

---
+++ modules/aaa/aaa.php
@@ -0,0 +1,23 @@
+<?php
+
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Aaa extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'aaa';
+        $this->version = '1.0.0';
+        $this->author = 'Tsilavina';
+        $this->secure_key = Tools::hash($this->name);
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Tsl module test');
+        $this->description = $this->l('Module test d\'apres√s tuto');
+    }
+}
---
+++ modules/aab/aab.php
@@ -0,0 +1,23 @@
+<?php
+
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Aab extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'aab';
+        $this->version = '1.0.0';
+        $this->author = 'Tsilavina';
+        $this->secure_key = Tools::hash($this->name);
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Tsl module test');
+        $this->description = $this->l('Module test d\'apres√s tuto');
+    }
+}
